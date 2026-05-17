b5ac768864d3eb145044aff61ca31e75c15d73f6
isaac-cli <garygutierrezprogramacion@gmail.com>
2026-05-16 22:24:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_eM5xpNGy6Smu@ep-fancy-glade-aq7yy3gy-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-    ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 app.get("/productos", async (req, res) => {
-    const result = await pool.query("SELECT * FROM productos");
-    res.json(result.rows);
+  const result = await pool.query("SELECT * FROM productos");
+  res.json(result.rows);
 });
 
 app.listen(3000, () => console.log("API running"));
